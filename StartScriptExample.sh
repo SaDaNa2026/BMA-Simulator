5ee6626f3da757b5f4bc286e6a20a296c491fa87
Samuel Namyslo <Samuel.Namyslo@fws.bwl.de>
2026-04-14 11:27:53 +0200

    Anpassung an die Umbenennung der Anwendung

--- StartScriptExample.sh
+++ StartScriptExample.sh
@@ -3,7 +3,7 @@
 # This is an example for a script that activates the virtual environment and launches the app.
 # Replace the project directory with the one you use.
 
-project_directory="/foo/bar/BMA-Steuerung"
+project_directory="/foo/bar/BMA-Simulator"
 
 cd  $project_directory || (echo "Starting bma_control failed: Unable to open specified project directory $project_directory"; exit)
 source .venv/bin/activate
